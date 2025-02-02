ecde157c903034889222b61b991d7f9e96443310
parich suriya <parich.suriya@gmail.com>
2025-02-02 12:32:49 +0700

    Job Seeker Form

--- app/actions.ts
+++ app/actions.ts
@@ -2,15 +2,13 @@
 
 import { z } from "zod";
 import { requireUser } from "./utils/hooks";
-import { companySchema } from "./utils/zodSchemas";
+import { companySchema, jobSeekerSchema } from "./utils/zodSchemas";
 import { prisma } from "./utils/db";
 import { redirect } from "next/navigation";
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const user = await requireUser();
   const validatedData = companySchema.parse(data);
-
-  console.log(validatedData);
 
   await prisma.user.update({
     where: {
@@ -20,6 +18,29 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
       onboardingCompleted: true,
       userType: "COMPANY",
       Company: {
+        create: {
+          ...validatedData,
+        },
+      },
+    },
+  });
+
+  return redirect("/");
+}
+
+export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
+  const user = await requireUser();
+
+  const validatedData = jobSeekerSchema.parse(data);
+
+  await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      onboardingCompleted: true,
+      userType: "JOB_SEEKER",
+      JobSeeker: {
         create: {
           ...validatedData,
         },
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,6 +1,4 @@
 import { auth } from "@/app/utils/auth";
-import { requireUser } from "@/app/utils/hooks";
-import { redirect } from "next/navigation";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
@@ -11,24 +9,47 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({
     image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
   })
     // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
+    .middleware(async () => {
       // This code runs on your server before upload
-      const session = await requireUser();
+      const session = await auth();
 
       // If you throw, the user will not be able to upload
-      if (!session.id) throw new UploadThingError("Unauthorized");
+      if (!session?.user) throw new UploadThingError("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: session.id };
+      return { userId: session.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+
+  resumeUploader: f({
+    "application/pdf": {
+      maxFileSize: "2MB",
+      maxFileCount: 1,
+    },
+  })
+    // Set permissions and file types for this FileRoute
+    .middleware(async () => {
+      // This code runs on your server before upload
+      const session = await auth();
+
+      // If you throw, the user will not be able to upload
+      if (!session?.user) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: session.user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
