20ef7794b47b45ef4fe84ca248bf9a9398f5d448
parich suriya <parich.suriya@gmail.com>
2025-02-03 10:02:17 +0700

    Post Job

--- app/actions.ts
+++ app/actions.ts
@@ -2,7 +2,7 @@
 
 import { z } from "zod";
 import { requireUser } from "./utils/hooks";
-import { companySchema, jobSeekerSchema } from "./utils/zodSchemas";
+import { companySchema, jobSchema, jobSeekerSchema } from "./utils/zodSchemas";
 import { prisma } from "./utils/db";
 import { redirect } from "next/navigation";
 import arcjet, { detectBot, shield } from "./utils/arcjet";
@@ -22,7 +22,6 @@ const aj = arcjet
   );
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const user = await requireUser();
-
   // Access the request object so Arcjet can analyze it
   const req = await request();
   // Call Arcjet protect
@@ -30,9 +29,7 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
   if (decision.isDenied()) {
     throw new Error("Forbidden");
   }
-
   const validatedData = companySchema.parse(data);
-
   await prisma.user.update({
     where: {
       id: user.id,
@@ -47,13 +44,11 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
       },
     },
   });
-
   return redirect("/");
 }
 
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   const user = await requireUser();
-
   // Access the request object so Arcjet can analyze it
   const req = await request();
   // Call Arcjet protect
@@ -61,9 +56,7 @@ export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   if (decision.isDenied()) {
     throw new Error("Forbidden");
   }
-
   const validatedData = jobSeekerSchema.parse(data);
-
   await prisma.user.update({
     where: {
       id: user.id,
@@ -78,6 +71,42 @@ export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
       },
     },
   });
+  return redirect("/");
+}
 
+export async function createJob(data: z.infer<typeof jobSchema>) {
+  const user = await requireUser();
+  // Access the request object so Arcjet can analyze it
+  const req = await request();
+  // Call Arcjet protect
+  const decision = await aj.protect(req);
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
+  const validatedData = jobSchema.parse(data);
+  const company = await prisma.company.findUnique({
+    where: {
+      userId: user.id,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (!company?.id) {
+    return redirect("/");
+  }
+  await prisma.jobPost.create({
+    data: {
+      companyId: company.id,
+      jobDescription: validatedData.jobDescription,
+      jobTitle: validatedData.jobTitle,
+      employmentType: validatedData.employmentType,
+      location: validatedData.location,
+      salaryFrom: validatedData.salaryFrom,
+      salaryTo: validatedData.salaryTo,
+      listingDuration: validatedData.listingDuration,
+      benefits: validatedData.benefits,
+    },
+  });
   return redirect("/");
 }
